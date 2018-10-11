14f67695ea67a836e0481a4a2dabec7e51931397
Paolo <paolo@fusefactory.it>
2018-10-11 15:33:31 +0200

    changed texture allocation

--- src/ofxSpout2Receiver.cpp
+++ src/ofxSpout2Receiver.cpp
@@ -8,10 +8,18 @@ Receiver::Receiver() {
 	;
 }
 
-void Receiver::setup() {
+void Receiver::setup(int width, int height) {
 	mReceiver = new SpoutReceiver;
 	mbInitialized = false;
 	SenderName[0] = 0;
+	receiving = false;
+
+	init(width, height);
+}
+
+void Receiver::init(int width, int height) {
+	mTexture.allocate(width, height, GL_RGBA);
+	mbInitialized = true;
 }
 
 void Receiver::showSenders() {
@@ -19,36 +27,68 @@ void Receiver::showSenders() {
 }
 
 void Receiver::updateTexture() {
-	if (!mbInitialized) {
-		unsigned int width, height;
+	unsigned int preWidth = mTexture.getWidth();
+	unsigned int preHeight = mTexture.getHeight();
+	unsigned int width, height;
+
+	if (!receiving) {
 		if (mReceiver->CreateReceiver(SenderName, width, height, true)) {
-			mTexture.allocate(width, height, GL_RGBA);
-			mbInitialized = true;
-			return;
+			if (width != preWidth || height != preHeight) { // in case of size change, reallocate
+				cout << "REALLOCATE FIRST TIME" << endl;
+				init(width, height);
+			}
+			receiving = true;
 		}
 		else {
 			ofLogWarning("ofxSpout", "No sender detected");
 		}
 	}
-
-	else { // mbInitialized
-		assert(mTexture.isAllocated() && "Texture not allocated but receiver initialized!");
-		unsigned int preWidth = mTexture.getWidth();
-		unsigned int preHeight = mTexture.getHeight();
-
-		unsigned int width, height;
+	else { // receiving
 		if (mReceiver->ReceiveTexture(SenderName, width, height, mTexture.getTextureData().textureID, mTexture.getTextureData().textureTarget)) {
 			if (width != preWidth || height != preHeight) { // in case of size change, reallocate
-				mTexture.allocate(width, height, GL_RGBA);
-				return;
+				cout << "REALLOCATE" << endl;
+				init(width, height);
+			}
+			else {
+				mReceiver->ReleaseReceiver();
+				receiving = false;
 			}
 		}
-		else {
-			// receiving failed
-			mReceiver->ReleaseReceiver();
-			mbInitialized = false;
-		}
 	}
+
+
+
+
+	//if (!mbInitialized) {
+	//	unsigned int width, height;
+	//	if (mReceiver->CreateReceiver(SenderName, width, height, true)) {
+	//		mTexture.allocate(width, height, GL_RGBA);
+	//		mbInitialized = true;
+	//		return;
+	//	}
+	//	else {
+	//		ofLogWarning("ofxSpout", "No sender detected");
+	//	}
+	//}
+
+	//else { // mbInitialized
+	//	assert(mTexture.isAllocated() && "Texture not allocated but receiver initialized!");
+	//	unsigned int preWidth = mTexture.getWidth();
+	//	unsigned int preHeight = mTexture.getHeight();
+
+	//	unsigned int width, height;
+	//	if (mReceiver->ReceiveTexture(SenderName, width, height, mTexture.getTextureData().textureID, mTexture.getTextureData().textureTarget)) {
+	//		if (width != preWidth || height != preHeight) { // in case of size change, reallocate
+	//			mTexture.allocate(width, height, GL_RGBA);
+	//			return;
+	//		}
+	//	}
+	//	else {
+	//		 receiving failed
+	//		mReceiver->ReleaseReceiver();
+	//		mbInitialized = false;
+	//	}
+	//}
 }
 
 void Receiver::exit() {
